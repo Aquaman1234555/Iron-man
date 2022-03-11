9f0ceaeb023dc86818b4ba84191881f3e327ae9d
Aquaman1234555 <101315879+Aquaman1234555@users.noreply.github.com>
2022-03-11 23:28:38 +0530

    Add files via upload

---
+++ scratch_canvas (1).py
@@ -0,0 +1,31 @@
+import turtle
+turtle.pencolor('blue')
+turtle.penup()
+turtle.setposition(-100, 100)
+turtle.pendown()
+turtle.pencolor()
+#write the color of pen in inverted commas
+turtle.pencolor('blue')
+turtle.circle(100)
+turtle.goto(-100, 100)
+turtle.penup()
+turtle.setposition()
+#write the location to fill the gap
+turtle.pendown()
+turtle.pencolor()
+#write the color of pen in inverted commas
+turtle.circle(200)
+#write the color of pen in inverted commas
+turtle.pencolor('blue')
+turtle.circle(100)
+turtle.goto(-200, 200)
+turtle.penup()
+turtle.pendown()
+#write the color of pen in inverted commas
+turtle.circle(200)
+#write the color of pen in inverted commas
+turtle.pencolor('blue')
+turtle.circle(100)
+turtle.goto(-200, 200)
+turtle.penup()
+turtle.pendown()
